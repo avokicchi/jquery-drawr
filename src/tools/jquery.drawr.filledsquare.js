60a233975ae2c8839a130947bd04e32a3f9351db
Lieuwe Prins <lprins@trumanscout.com>
2026-03-01 09:30:49 +0100

    resolves #61

--- src/tools/jquery.drawr.filledsquare.js
+++ src/tools/jquery.drawr.filledsquare.js
@@ -26,7 +26,24 @@ jQuery.fn.drawr.register({
 		context.lineJoin = 'miter';
 		context.lineWidth = size;
 		context.fillStyle = "rgb(" + this.brushColor.r + "," + this.brushColor.g + "," + this.brushColor.b + ")";
-		context.fillRect(brush.startPosition.x,brush.startPosition.y,brush.currentPosition.x-brush.startPosition.x,brush.currentPosition.y-brush.startPosition.y);
+		var angle = this.rotationAngle || 0;
+		var sx = brush.startPosition.x, sy = brush.startPosition.y;
+		var ex = brush.currentPosition.x, ey = brush.currentPosition.y;
+		if(angle){
+			var cx = this.width/2, cy = this.height/2;
+			var cos = Math.cos(angle), sin = Math.sin(angle);
+			context.save();
+			context.translate(cx, cy);
+			context.rotate(-angle);
+			context.translate(-cx, -cy);
+			var dsx = sx-cx, dsy = sy-cy, dex = ex-cx, dey = ey-cy;
+			sx = cx + cos*dsx - sin*dsy;
+			sy = cy + sin*dsx + cos*dsy;
+			ex = cx + cos*dex - sin*dey;
+			ey = cy + sin*dex + cos*dey;
+		}
+		context.fillRect(sx, sy, ex-sx, ey-sy);
+		if(angle){ context.restore(); }
 
 		this.effectCallback = null;
 		return true;
@@ -38,11 +55,36 @@ jQuery.fn.drawr.register({
 		};
 	},
 	effectCallback: function(context,brush,adjustx,adjusty,adjustzoom){
+		var angle = this.rotationAngle || 0;
+		var sx, sy, ex, ey;
+		if(angle){
+			var _W = this.width * adjustzoom;
+			var _H = this.height * adjustzoom;
+			var _cx = _W / 2 - adjustx;
+			var _cy = _H / 2 - adjusty;
+			context.save();
+			context.translate(_cx, _cy);
+			context.rotate(-angle);
+			context.translate(-_cx, -_cy);
+			var cos = Math.cos(angle), sin = Math.sin(angle);
+			var halfW = this.width * adjustzoom / 2, halfH = this.height * adjustzoom / 2;
+			var sRelX = brush.startPosition.x  - this.width/2, sRelY = brush.startPosition.y  - this.height/2;
+			var eRelX = brush.currentPosition.x - this.width/2, eRelY = brush.currentPosition.y - this.height/2;
+			sx = (cos*sRelX - sin*sRelY) * adjustzoom + halfW - adjustx;
+			sy = (sin*sRelX + cos*sRelY) * adjustzoom + halfH - adjusty;
+			ex = (cos*eRelX - sin*eRelY) * adjustzoom + halfW - adjustx;
+			ey = (sin*eRelX + cos*eRelY) * adjustzoom + halfH - adjusty;
+		} else {
+			sx = brush.startPosition.x  * adjustzoom - adjustx;
+			sy = brush.startPosition.y  * adjustzoom - adjusty;
+			ex = brush.currentPosition.x * adjustzoom - adjustx;
+			ey = brush.currentPosition.y * adjustzoom - adjusty;
+		}
 		context.globalAlpha=brush.currentAlpha;
 		context.lineJoin = 'miter';
-		//context.lineWidth = this.brushSize;
 		context.fillStyle = "rgb(" + this.brushColor.r + "," + this.brushColor.g + "," + this.brushColor.b + ")";
-		context.fillRect((brush.startPosition.x*adjustzoom)-adjustx,(brush.startPosition.y*adjustzoom)-adjusty,(brush.currentPosition.x-brush.startPosition.x)*adjustzoom,(brush.currentPosition.y-brush.startPosition.y)*adjustzoom);
+		context.fillRect(sx, sy, ex-sx, ey-sy);
+		if(angle){ context.restore(); }
 	}
 });
 
